35b1a3f3811bf42d489a2121e91c6dcd32cc974b
Ebony Rose Martin <19314744+AlphaRose@users.noreply.github.com>
2026-01-05 15:16:21 -0500

    Add script to hide 365 Group and disabled mailboxes
    This script hides a specified 365 Group and all disabled user mailboxes from address lists in Outlook.

---
+++ powershell/scripts/365 Group Hide from Outlook.ps1
@@ -0,0 +1,10 @@
+#365 Group Hide from Outlook.ps1
+#For a 365 Group
+Set-UnifiedGroup -Identity "Legal Department" -HiddenFromAddressListsEnabled $true -HiddenFromExchangeClientsEnabled
+
+#For all disabled accounts
+$mailboxes = get-user | where {$_.UserAccountControl -like '*AccountDisabled*' -and $_.RecipientType -eq 'UserMailbox' } | get-mailbox | where {$_.HiddenFromAddressListsEnabled -eq $false}
+foreach ($mailbox in $mailboxes) { Set-Mailbox -HiddenFromAddressListsEnabled $true -Identity $mailbox }
+
+#For a single mailbox
+Set-Mailbox -Identity "UserMailbox" -HiddenFromAddressListsEnabled $true
